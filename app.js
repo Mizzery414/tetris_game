c1c459a2848ebf908bb58d9495b809a0163126d7
Melanie Rowsey-McWilliams <melanie.rowsey@gmail.com>
2022-04-15 19:45:49 -0700

    add score and hide taken tetrominoes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const startBtn = document.querySelector("#start-button");
   let nextRandom = 0;
   let timerId = "";
+  let score = 0;
 
   const width = 10;
   console.log(squares);
@@ -116,6 +117,7 @@ document.addEventListener("DOMContentLoaded", () => {
       currentPosition = 4;
       draw();
       displayShape();
+      addScore();
     }
   }
 
@@ -204,4 +206,34 @@ document.addEventListener("DOMContentLoaded", () => {
       displayShape();
     }
   });
+
+  //add score
+  function addScore() {
+    for (let i = 0; i < 199; i += width) {
+      const row = [
+        i,
+        i + 1,
+        i + 2,
+        i + 3,
+        i + 4,
+        i + 5,
+        i + 6,
+        i + 7,
+        i + 8,
+        i + 9,
+      ];
+
+      if (row.every((index) => squares[index].classList.contains("taken"))) {
+        score += 10;
+        scoreDisplay.innerHTML = score;
+        row.forEach((index) => {
+          squares[index].classList.remove("taken");
+          squares[index].classList.remove("tetromino");
+        });
+        const squaresRemoved = squares.splice(i, width);
+        squares = squaresRemoved.concat(squares);
+        squares.forEach((cell) => grid.appendChild(cell));
+      }
+    }
+  }
 });
